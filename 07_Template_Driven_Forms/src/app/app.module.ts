c09a9af98bfc3f94bb57161a730e0639b741e950
Jock Dalby <jockdalby86@gmail.com>
2017-04-25 10:16:39 +1200

    register form controls with Angular using ngModel

--- 07_Template_Driven_Forms/src/app/app.module.ts
+++ 07_Template_Driven_Forms/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    // Important to add FormsModule when using <form>
     FormsModule,
     HttpModule
   ],
