4e184f4f5d6d71d3167374f0059821b711af4f58
Jock Dalby <jockdalby86@gmail.com>
2017-04-26 08:49:25 +1200

    add formControls to FormGroup and connect to HTML template

--- 08_reactive_forms/src/app/app.component.ts
+++ 08_reactive_forms/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   genders = ['male', 'female'];
 
   signupForm: FormGroup;
+
+  ngOnInit() {
+    // Create a new FormGroup by passing an object of key:value pairs
+      // Property names are wrapped in quotations to protect them during minification. This may not be necessary and without them the property names may not get destroyed, but just to be sure, we are wrapping them as strings.
+     // We can pass FormControl up to 3 arguments (initialState, single validator/array of validators, potential async validators).
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+
+    });
+  }
 }
