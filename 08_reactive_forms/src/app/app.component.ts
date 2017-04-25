387cb527217a4541a66d1ff9210ce00a9bc3aa44
Jock Dalby <jockdalby86@gmail.com>
2017-04-26 09:30:12 +1200

    add nested validation

--- 08_reactive_forms/src/app/app.component.ts
+++ 08_reactive_forms/src/app/app.component.ts
@@ -16,6 +16,10 @@ export class AppComponent implements OnInit{
       // Property names are wrapped in quotations to protect them during minification. This may not be necessary and without them the property names may not get destroyed, but just to be sure, we are wrapping them as strings.
      // We can pass FormControl up to 3 arguments (initialState, single validator/array of validators, potential async validators).
     this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+      }),
       'username': new FormControl(null, Validators.required),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
