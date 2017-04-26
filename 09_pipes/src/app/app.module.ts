570c25cc02bc43d519f9f3b7ed97c746cb84d106
Jock Dalby <jockdalby86@gmail.com>
2017-04-26 12:49:26 +1200

    add custom shortenWithParams pipe

--- 09_pipes/src/app/app.module.ts
+++ 09_pipes/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ShortenPipe } from "./shorten.pipe";
+import { ShortenWithParamsPipe } from "./shortenWithParams.pipe";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShortenPipe
+    ShortenPipe,
+    ShortenWithParamsPipe
   ],
   imports: [
     BrowserModule,
