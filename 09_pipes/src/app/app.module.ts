31c4c05d878c60080d1ad78f874f393b87a201ca
Jock Dalby <jockdalby86@gmail.com>
2017-04-26 13:06:17 +1200

    add pipe for filtering

--- 09_pipes/src/app/app.module.ts
+++ 09_pipes/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from "./filter.pipe";
 import { ShortenPipe } from "./shorten.pipe";
 import { ShortenWithParamsPipe } from "./shortenWithParams.pipe";
 
 @NgModule({
   declarations: [
     AppComponent,
+    FilterPipe,
     ShortenPipe,
     ShortenWithParamsPipe
   ],
