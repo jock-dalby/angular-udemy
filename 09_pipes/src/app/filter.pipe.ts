2ab521e58b041cf3834552036fb637a937a51a58
Jock Dalby <jockdalby86@gmail.com>
2017-04-26 13:17:13 +1200

    add pure:false to pipe settings to improve functionality

--- 09_pipes/src/app/filter.pipe.ts
+++ 09_pipes/src/app/filter.pipe.ts
@@ -1,7 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'filter'
+    name: 'filter',
+    pure: false
+    // pure: false means that whenever we change data on the page our pipe will be re-calculated. This is not advisable unless really necessary because can cost quite a bit of performance time. To see the difference it makes, type jock into search bar and then click add server. When the server is added, the data going into the pipe changes and re-triggers the pipe calculation.
 })
 
 export class FilterPipe implements PipeTransform {
@@ -12,7 +14,7 @@ export class FilterPipe implements PipeTransform {
         }
         const resultArray = [];
         for (const item of value) {
-            if (item[propName].includes(filterString)) {
+            if (item[propName].toLowerCase().includes(filterString.toLowerCase())) {
                 resultArray.push(item);
             }
         }
